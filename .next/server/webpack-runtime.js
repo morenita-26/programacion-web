4e13d19542a105e5fb0fa0cdc618680ab4c5a2e5
Morena <mfuxmanarroyo@itba.edu.ar>
2026-05-05 20:20:19 -0300

    arreglo del banner

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9c8760d779272b64")
+/******/ 		__webpack_require__.h = () => ("21d6748bd522ba32")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c4a680f4540d60e0"; }
+/******/ 		__webpack_require__.h = function() { return "a43578128347985a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
